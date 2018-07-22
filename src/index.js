b3db02cf0b612aab7af2ec7e154f59a3293244c1
GyGaByyyTe <gygabyyyte@gmail.com>
2018-07-22 19:09:26 +0300

    fixed initialization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import initMap from "./map";
+import { initMap } from './map';
 
 ymaps.ready(() => {
-  initMap(ymaps, "map");
-  console.log("inited");
+  initMap(ymaps, 'map');
+  console.log('inited');
 });
