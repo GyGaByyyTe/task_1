b446b8c041f4e3ea03834edf899c17a962fbe156
GyGaByyyTe <gygabyyyte@gmail.com>
2018-07-24 00:50:34 +0300

    fixed cluster colors

--- src/map.js
+++ src/map.js
@@ -19,7 +19,6 @@ export function initMap(ymaps, containerId) {
     geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
   });
 
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
   myMap.geoObjects.add(objectManager);
 
   loadList().then(data => {
